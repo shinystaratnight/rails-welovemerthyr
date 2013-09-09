2b78e04c004d9b64447441240037f7eaa637646a
Giang NGUYEN <ndgiang84@gmail.com>
2013-09-10 03:22:34 +0700

    Added rake task.

--- lib/import_seed_data.rb
+++ lib/import_seed_data.rb
@@ -6,30 +6,13 @@ def import_businesses
   file_path = Rails.root.join('db/seeds/businesses.csv')
 
   CSV.read(file_path, headers: true).each do |csv_row|
-    weekly_schedule = {}
-
     new_row = csv_row.to_hash.except("Stamp").inject({}) do |row, (k, v)|
-      if k.present? && !k.downcase.ends_with?("opening") && !k.downcase.ends_with?("closing")
-        if k != 'Services'
-          row[k.downcase.to_sym] = v
-        else
-          row[:service_list] = v
-        end
-      elsif k.present? && (k.downcase.ends_with?("opening") || k.downcase.ends_with?("closing"))
-        weekly_schedule[k.downcase.parameterize.underscore] = v
-      end
-
+      row[k.downcase.parameterize.underscore.to_sym] = v if k.present?
       row
     end
 
     b = Business.new(new_row)
 
-    if b.save
-      b.create_weekly_schedule!(weekly_schedule)
-    else
-      print '*'
-    end
-
-    print '.'
+    b.save ?  print('.') : print('*')
   end
 end
