eb382f18107b0ed92778eadc38ea5508ba56a75a
Giang NGUYEN <ndgiang84@gmail.com>
2013-09-11 02:54:40 +0700

    config

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,18 +1,20 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
-    b.use :html5
+  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group form-group', :error_class => 'error has-error' do |b|
+#    b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
+    b.use :label, class: 'control-label col-lg-2'
+    b.wrapper :tag => 'div', :class => 'controls col-lg-10' do |ba|
       ba.use :input
-      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline has-error' }
       ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+#      ba.wrapper :tag => 'input', type: 'checkbox', :class => 'form-control' do |input|
+#      end
     end
   end
 
   config.wrappers :prepend, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
-    b.use :html5
+#    b.use :html5
     b.use :placeholder
     b.use :label
     b.wrapper :tag => 'div', :class => 'controls' do |input|
@@ -25,7 +27,7 @@ SimpleForm.setup do |config|
   end
 
   config.wrappers :append, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
-    b.use :html5
+#    b.use :html5
     b.use :placeholder
     b.use :label
     b.wrapper :tag => 'div', :class => 'controls' do |input|
