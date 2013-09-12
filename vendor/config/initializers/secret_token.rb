289b33ea17165a992fcc6011457d62c2f78e30c4
Giang NGUYEN <ndgiang84@gmail.com>
2013-09-12 23:53:42 +0700

    Removed a bunches of dupicated files

--- vendor/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Welovemerthyr::Application.config.secret_token = '15fdc9558d52b3c9d98c51ed794864eecd5428184b3d52e440812b6c0923c542ff5b11fca5aeaae24bb321e2a81e8ca1f4e554f54934bdc132cbad5017775fb7'
