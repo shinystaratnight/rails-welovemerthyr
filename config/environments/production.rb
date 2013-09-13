6f88e2c00e81c0ea8965ce199f14cfd93cd7aa3e
Giang NGUYEN <ndgiang84@gmail.com>
2013-09-14 00:40:21 +0700

    Added default_url_options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Welovemerthyr::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'welovemerthyr.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'welovemerthyr.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
