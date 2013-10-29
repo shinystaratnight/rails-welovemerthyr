86ff3a4e0bb1682536a05ae0307e19bd8f526d68
Giang NGUYEN <ndgiang84@gmail.com>
2013-10-29 18:06:01 +0700

    Added URL to sliders.

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -4,10 +4,11 @@ class Slider
   field :caption, type: String
   field :order, type: Integer
   field :image, type: String
+  field :url, type: String
 
   validates_presence_of :image, :caption
 
-  attr_accessible :image, :caption, :order, :remove_image
+  attr_accessible :image, :caption, :order, :remove_image, :url
 
   mount_uploader :image, SliderImageUploader
 end
