a0b0233a87234c46e8baff17f895c5607e82f0db
Giang NGUYEN <ndgiang84@gmail.com>
2013-10-30 00:07:54 +0700

    Added ability to choose layout for page template.

--- app/models/page_template.rb
+++ app/models/page_template.rb
@@ -2,8 +2,11 @@ class PageTemplate
   include Mongoid::Document
   include Mongoid::Slug
 
+  LAYOUT_NAMES = %w(application with_sidebar)
+
   field :title, type: String
   field :body, type: String
+  field :layout_name, type: String, default: LAYOUT_NAMES[0]
 
   slug :title
 
@@ -11,5 +14,5 @@ class PageTemplate
 
   validates_presence_of :title
 
-  attr_accessible :title, :body
+  attr_accessible :title, :body, :layout_name
 end
