6164aae3180d2e4e4489dd6fd289d4f3879a9660
Giang NGUYEN <ndgiang84@gmail.com>
2014-03-22 00:23:14 +0700

    Added ability to hide some sliders.

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -6,12 +6,14 @@ class Slider
   field :order, type: Integer
   field :image, type: String
   field :url, type: String
+  field :visible, type: Boolean, default: true
 
   validates_presence_of :image
 
-  attr_accessible :image, :caption, :order, :remove_image, :url
+  attr_accessible :image, :caption, :order, :remove_image, :url, :visible
 
   mount_uploader :image, SliderImageUploader
 
   scope :newest, desc(:created_at)
+  scope :visible, where(:visible.ne => false)
 end
