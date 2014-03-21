7251dea32b988dddebe6c4a97f69d359dd6841ed
Giang NGUYEN <ndgiang84@gmail.com>
2014-03-22 00:34:51 +0700

    Added zone to businesses.

--- app/models/business.rb
+++ app/models/business.rb
@@ -17,6 +17,7 @@ class Business
                     6am 6.30am 7am 7.30am 8am 8.30am 9am 9.30am 10am 10.30am 11am 11.30am
                     12pm 12.30pm 1pm 1.30pm 2pm 2.30pm 3pm 3.30pm 4pm 4.30pm 5pm 5.30pm
                     6pm 6.30pm 7pm 7.30pm 8pm 8.30pm 9pm 9.30pm 10pm 10.30pm 11pm 11.30pm)
+  ZONES        = ['Main BID', 'Pontmorlais', 'Other']
 
   field :name, type: String
   field :category, type: String
@@ -33,6 +34,7 @@ class Business
   field :profile, type: String
   field :photo, type: String
   field :coordinates, type: Array
+  field :zone, type: String, default: ZONES[0]
 
   slug :name
 
@@ -53,7 +55,7 @@ class Business
                   :monday_opening, :monday_closing, :tuesday_opening, :tuesday_closing,
                   :wednesday_opening, :wednesday_closing, :thursday_opening,
                   :thursday_closing, :friday_opening, :friday_closing, :saturday_opening,
-                  :saturday_closing, :sunday_opening, :sunday_closing
+                  :saturday_closing, :sunday_opening, :sunday_closing, :zone
 
   geocoded_by :full_address
   after_validation :geocode, if: ->{ address_changed? || town_changed? || postcode_changed? }
