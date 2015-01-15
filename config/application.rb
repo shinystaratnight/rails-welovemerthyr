85a434d7ea1d66d2a23c9e010558ecb7b3d699ba
Artyom Lagun <artyom.lagun@gmail.com>
2015-01-15 13:21:49 +0300

    fix compile assets

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module Welovemerthyr
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     config.generators do |g|
       g.factory_girl false
