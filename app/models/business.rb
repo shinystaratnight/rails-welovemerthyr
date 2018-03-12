328492260e258c946bb6b768b5ac776cd03f1c5a
Dipak <dade.dipakg@gmail.com>
2018-03-12 11:32:45 +0530

    Search Issue revert

--- app/models/business.rb
+++ app/models/business.rb
@@ -97,26 +97,11 @@ class Business
   after_save do
     update_index
   end
-
-  settings :analysis=> {
-    :analyzer=> {
-       :custom_analyzer=> {
-         :tokenizer => 'standard',
-         :filter => ['standard', 'lowercase', 'stop', 'snowball', 'ngram', "stemmer"],
-         :type => 'custom' }
-     }
-   } do
-
-    mapping do
-      indexes :name, :analyzer => 'custom_analyzer'
-      indexes :services, :analyzer => 'custom_analyzer'
-    end
+  
+  mapping do
+    indexes :name, :analyzer => 'snowball'
+    indexes :services, :analyzer => 'snowball'
   end
-
-  # mapping do
-  #   indexes :name, :analyzer => 'snowball'
-  #   indexes :services, :analyzer => 'snowball'
-  # end
 
   def to_indexed_json
     {
